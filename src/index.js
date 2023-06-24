750e9cd8c3f855cb3f94b352d78924cccabc7a6a
IvashkaVIP <ivashka.co@gmail.com>
2023-06-24 10:21:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import './index.css';
 // const element = React.createElement('div',{a:5,b:0},'Hello World!')
 // console.log(element);
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
