b3629f39fb3501483839cb385f662bc113e5fd6a
IvashkaVIP <ivashka.co@gmail.com>
2023-06-24 15:51:49 +0300

    add component profile

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Profile from 'components/profile';
+import user from './user.json'
+
+const App = () => {
+  return (
+    <div>
+      <Profile 
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+    />
+      
+    </div>
+  );
+};
+
+export default App;
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
